69cc598dee16da0f9b6de5f6c20aa533e151b40d
Renjusha <ren1516r@gmail.com>
2025-05-19 12:10:27 +0400

    initial

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
+    //names venue,av,meals are store state name, it should same in Useselector
     venue: venueReducer,
+    av:avReducer,
+    meals:mealsReducer
   },
 });
---
+++ src/venue.cjs
@@ -0,0 +1,5 @@
+//import { incrementQuantity } from "./venueSlice.js";
+import storea from './store';
+
+//const store = require('store.js');
+console.log(storea.getState());
